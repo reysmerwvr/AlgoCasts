344514350724cc0cbcab77b2f34c87e9cc6aacc7
Reysmer Valle <reysmerwvr@gmail.com>
2021-02-16 20:45:47 -0500

    Add reverse string solution

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # AlgoCasts
 
 Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/)
+
+
+## Usefull commands
+
+### Run Debugger
+
+```bash
+> node inspect reversestring/index.js
+> c
+> repl
+> str
+```
+
+### Run Test Suite
+
+```bash
+> jest reversestring/test.js --watch
+```
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Option 1 - Python equivalent
+// def reverse(string):
+//     return string[::-1]
+
+// Option 1 - Javascript
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Option 2 - Python equivalent
+// def reverse(string):
+//     rev = ''
+//     for char in string:
+//         rev = char + rev
+//     return rev
+
+// Option 2 - Javascript
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str) {
+//         reversed = character + reversed; 
+//     }
+//     return reversed;
+// }
+
+// Option 3 - Javascript
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+reverse('hello');
 
 module.exports = reverse;
