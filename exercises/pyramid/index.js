209a8589ebea5bd4a274b4e5f6aee1a5d1ce1be4
Reysmer Valle <reysmerwvr@gmail.com>
2021-02-20 12:56:43 -0500

    Add steps and pyramid solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Option 1 - Javascript
+// function pyramid(n) {
+//   const columns = 2 * n - 1;
+//   const midpoint = Math.floor(columns / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let column = 0; column < columns; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// Option 2 - Javascript (Recursive)
+function pyramid(n, row = 0, level = '') {
+  if(row === n) {
+    return;
+  }
+
+  const columns = 2 * n - 1;
+
+  if (level.length === columns) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor(columns / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,53 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Option 1 - Javascript
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#'
+//             } else {
+//                 stair += ' '
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
+
+// Option 2 - Javascript (Recursive)
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
+
+
+// function arrayLista(arr) {
+//   const lista = {}
+//   if(arr.length <= 0) {
+//     return lista;
+//   } else if(arr.length === 1) {
+//     lista.valor = arr.shift();
+//     lista.resto = null;
+//     return lista;
+//   } else {
+//     lista.valor = arr.shift();
+//     lista.resto = arrayLista(arr); 
+//   }
+//   return lista;
+// }
+
+// console.log(arrayLista([1, 2, 3]));
