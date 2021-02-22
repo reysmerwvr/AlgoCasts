ae3fe3fc36f7c6ab5266fd77ed30cc617599b532
Reysmer Valle <reysmerwvr@gmail.com>
2021-02-21 19:20:27 -0500

    Add queue, stack solutions

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,67 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+// Solution
+// class Queue {
+//   constructor() {
+//     this.stackOne = new Stack();
+//     this.stackTwo = new Stack();
+//   }
+
+//   add(record) {
+//     if (!this.stackOne.peek()) {
+//       this.stackOne.push(record);
+//     } else if (!this.stackTwo.peek()) {
+//       this.stackTwo.push(this.stackOne.pop());
+//       this.stackOne.push(record);
+//     } else {
+//       const tempElement = this.stackTwo.pop();
+//       this.stackTwo.push(this.stackOne.pop());
+//       this.stackTwo.push(tempElement);
+//       this.stackOne.push(record);
+//     }
+//   }
+
+//   remove() {
+//     return (this.stackTwo.peek()) ? this.stackTwo.pop() : this.stackOne.pop();
+//   }
+
+//   peek() {
+//     return (this.stackTwo.peek()) ? this.stackTwo.peek() : this.stackOne.peek();
+//   }
+// }
+
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record =  this.second.pop();
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record =  this.second.peek();
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
