1657792d651f314e24719f6b5ebcf0b2f7b96761
Reysmer Valle <reysmerwvr@gmail.com>
2021-02-22 20:53:13 -0500

    Add tree and binary tree solutions

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	const delimiter = 's';
+	const arr = [root, delimiter];
+	const counters = [0];
+	while (arr.length > 1) {
+		const node = arr.shift();
+		
+		if (node === delimiter) {
+			counters.push(0);
+			arr.push(delimiter)
+		} else {
+			arr.push(...node.children);
+			counters[counters.length - 1]++;
+		}
+	}
+
+	return counters;
+}
 
 module.exports = levelWidth;
