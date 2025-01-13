fa914f11bf7343c03b1981d25b5a36e5ef16b3e8
Reysmer Valle <reysmerwvr@gmail.com>
2025-01-13 23:54:58 +0100

    add singly linked list data structure

---
+++ data_structures/linked_list/singly_linked_list.js
@@ -0,0 +1,25 @@
+const SinglyLinkedListNode = class {
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
+};
+
+const SinglyLinkedList = class {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertNode(nodeData) {
+    const node = new SinglyLinkedListNode(nodeData);
+
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+  }
+};
